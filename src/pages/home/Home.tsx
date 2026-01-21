8e867ec6b17b71d6e6eab1b2b19f9541597b1e23
Juan Felipe Ospina <jospina@almeraim.com>
2026-01-21 15:17:13 -0500

    AppProvider first implementation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,12 @@
+import { useAppContext } from '../../context/AppContext'
+
 export function Home() {
+  const { theme, toggleTheme } = useAppContext()
+  
   return (
-    <div className="page">
-      <h2>Home Page</h2>
-      <p>Esta es la página de inicio de tu aplicación React.</p>
+    <div className={`page ${theme}`}>
+      <h2>Home Page - Tema: {theme}</h2>
+      <button onClick={toggleTheme}>Cambiar tema</button>
     </div>
-  );
+  )
 }
